3311837047045b392c234f674037ecf46862a6b0
AnnaBalinov <98451117+AnnaBalinov@users.noreply.github.com>
2022-01-27 00:45:43 +0200

    Update

--- js/mainGame.js
+++ js/mainGame.js
@@ -2,29 +2,34 @@
 
 const MINE = '🌸'
 const FLOOR = ' '
-const MINE_IMG = '`<img class="wall" src="img/bomb.png">`;'
+const MINE_IMG = `<img class="bomb" src="img/bomb.png">`
+const COVER = `<img class="cover" src="img/cover.png">`
 
 var gGame = {
     score: 0,
     isOn: false,
-    // boardSize: 4
+}
+
+var gLevel = {
+    SIZE: 4,
+    MINES: 2
 }
 
 var gBoard = []
+var gTime = 0
+var gTimeInterval;
 
-function initGame() {
-    gBoard = buildBoard(4)
+function initGame(level, mines) {
+    gBoard = buildBoard(level, mines)
     renderBoard(gBoard)
-    setMines(gBoard)
     console.table(gBoard)
 }
-
 
 // Builds the board - V
 // Set mines at random locations - V
 // Call setMinesNebsCount() - V
 // Return the created board - V
-function buildBoard(size = 4) {
+function buildBoard(size = 4, level = 2) {
     var board = []
     for (var i = 0; i < size; i++) {
         board.push([])
@@ -36,31 +41,29 @@ function buildBoard(size = 4) {
                 isMarked: false
             }
             board[i][j] = cell
-
-            //set mines
-            var randomLocation = setMines(board)
-            if (i === randomLocation.i && j === randomLocation.j) {
-                board[i][j].isMine = true
-            }
         }
     }
+    //set mines
+    for (var i = 0; i < level; i++) {
+        var location = randomCellLocation(board)
+        board[location.i][location.j].isMine = true
+    }
+
     return board
 }
 
 
-
 //set random mines on board 
-function setMines(board) {
+function randomCellLocation(board) {
     var cells = []
-    for (var i = 0; i < board.length; i++) {
-        for (var j = 0; j < board[0].length; j++) {
+    for (var i = 0; i < board.length - 1; i++) {
+        for (var j = 0; j < board[0].length - 1; j++) {
             cells.push({ i: i, j: j })
         }
     }
     var cellLocation = cells[getRandomIntInclusive(0, cells.length - 1)]
     return cellLocation
 }
-
 
 
 //Count mines around each cell - V
@@ -86,10 +89,10 @@ function renderBoard(board) {
     for (var i = 0; i < board.length; i++) {
         strHTML += '<tr>\n'
         for (var j = 0; j < board[0].length; j++) {
-            var currCell = FLOOR
-            if (board[i][j].isMine) {
-                currCell = MINE
-            }
+            var currCell = COVER
+            // if (board[i][j].isMine) {
+            //     currCell = MINE
+            // }
             var cellClass = `cell-${i}-${j}`
             var cellId = `i:${i}, j:${j}`
             strHTML += `<td id="${cellId}" onclick="cellClicked(this, ${i}, ${j})" class="${cellClass}">`
@@ -114,7 +117,17 @@ function cellClicked(elCell, i, j) {
     gBoard[i][j].minesAroundCount = setMinesNebsCount(gBoard, i, j)
 
     // // update the dom
-    elCell.innerText = gBoard[i][j].minesAroundCount
+    if (gBoard[i][j].minesAroundCount !== 0) {
+        elCell.innerText = gBoard[i][j].minesAroundCount
+    } else {
+        elCell.innerText = FLOOR
+    }
+    if (gBoard[i][j].isMine) {
+        elCell.innerText = MINE
+    }
+
+
+    setTimer()
 }
 
 
@@ -127,9 +140,27 @@ function expandShown(board, elCell, i, j) {
 }
 
 
-
-
-
 function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+//timer
+function setTimer() {
+    if (gGame.isOn) return
+    gGame.isOn = true
+    gTimeInterval = setInterval(renderTime, 10)
+    gTime = Date.now()
+}
+
+function renderTime() {
+    var now = Date.now()
+    var time = ((now - gTime) / 1000).toFixed(2)
+    var elTime = document.querySelector('.timer')
+    elTime.innerHTML = time
+}
+
+function stopTimer() {
+    clearInterval(gTimeInterval)
+    gTimeInterval = 0
 }
